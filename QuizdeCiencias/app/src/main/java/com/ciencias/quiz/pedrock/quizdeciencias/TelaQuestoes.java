cd4e543c84c2e255e68d161bae5a18ac9a081668
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-02-23 16:05:06 -0300

    adição de AlertDialog ao clicar no btn voltar

--- QuizdeCiencias/app/src/main/java/com/ciencias/quiz/pedrock/quizdeciencias/TelaQuestoes.java
+++ QuizdeCiencias/app/src/main/java/com/ciencias/quiz/pedrock/quizdeciencias/TelaQuestoes.java
@@ -1,23 +1,23 @@
 package com.ciencias.quiz.pedrock.quizdeciencias;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ciencias.quiz.pedrock.quizdeciencias.helper.DbHelper;
 
 
 public class TelaQuestoes extends AppCompatActivity {
-
 
     private Cursor cursor;
     private TextView questao;
@@ -81,6 +81,40 @@ public class TelaQuestoes extends AppCompatActivity {
 
     }
 
+    // AlertDialog após clicar no botão voltar
+    @Override
+    public void onBackPressed() {
+
+        // Cria Alerta
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        // Nomeia as mensagens
+        alert.setTitle("Você deseja sair ?");
+        alert.setMessage("Se vocẽ sair, seu progresso será perdido!");
+
+        // Configura cancelamento
+        alert.setCancelable(false);
+
+        // Configura ícone
+        alert.setIcon(android.R.drawable.ic_dialog_alert);
+
+        alert.setPositiveButton("Ficar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        alert.setNegativeButton("Sair", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        alert.create();
+        alert.show();
+    }
 
     public void avancarQuestao(View view) {
 
