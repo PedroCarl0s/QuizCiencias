08fd14c21279175d2836cf61bd677237a8858e41
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-02-23 10:40:26 -0300

    recuperando questões e modificando o enunciado

--- QuizdeCiencias/app/src/main/java/com/ciencias/quiz/pedrock/quizdeciencias/TelaQuestoes.java
+++ QuizdeCiencias/app/src/main/java/com/ciencias/quiz/pedrock/quizdeciencias/TelaQuestoes.java
@@ -1,15 +1,23 @@
 package com.ciencias.quiz.pedrock.quizdeciencias;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.ciencias.quiz.pedrock.quizdeciencias.helper.DbHelper;
 
 public class TelaQuestoes extends AppCompatActivity {
 
 
     private TextView titulo;
     private ImageView imgTitulo;
+    private Button btnProxima;
 
     private String enunciado;
     private String letraA, letraB, letraC, letraD;
@@ -40,6 +48,27 @@ public class TelaQuestoes extends AppCompatActivity {
         titulo = findViewById(R.id.lblTitulo);
         titulo.setText(nome);
 
+
+
+        String nomeTabela = "";
+        if (nome.equalsIgnoreCase("Química")) nomeTabela = DbHelper.TABLE_QUIMICA;
+        else if (nome.equalsIgnoreCase("Física")) nomeTabela = DbHelper.TABLE_FISICA;
+        else nomeTabela = DbHelper.TABLE_BIOLOGIA;
+
+        DbHelper db = new DbHelper(getApplicationContext());
+
+        String sql = "SELECT * FROM " + nomeTabela + " ;";
+        SQLiteDatabase banco = db.getReadableDatabase();
+
+        Cursor cursor = banco.rawQuery(sql, null);
+
+        //btnProxima = (Button) findViewById(R.id.btnAvancar);
+
+        cursor.moveToFirst();
+
+        TextView questao = (TextView) findViewById(R.id.lblEnunciado);
+        questao.setText(cursor.getString(0) + ") " + cursor.getString(1));
+
     }
 
 
@@ -51,5 +80,7 @@ public class TelaQuestoes extends AppCompatActivity {
         else if (titulo.equalsIgnoreCase("Biologias")) return imgID[2];
         else throw new Exception("Erro: título inválido!");
     }
+
+
 
 }
