a5e685638d8ea87933f8e247b49977bebc999cd4
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-02-26 02:42:36 -0300

    Adição da classe para criar efeito splash

---
+++ QuizdeCiencias/app/src/main/java/com/ciencias/quiz/pedrock/quizdeciencias/TelaSplash.java
@@ -0,0 +1,33 @@
+package com.ciencias.quiz.pedrock.quizdeciencias;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class TelaSplash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_splash);
+
+        // Deixar em tela cheia
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+
+
+    }
+}
